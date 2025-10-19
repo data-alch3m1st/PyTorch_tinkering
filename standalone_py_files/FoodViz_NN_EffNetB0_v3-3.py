3701282545e33c68e259b021e196620eab8236eb
data-alch3m1st <d4t4.4lch3my+g1thub@gmail.com>
2025-10-19 11:36:59 -0400

    Update FoodViz_NN_EffNetB0_v3-3.py

--- standalone_py_files/FoodViz_NN_EffNetB0_v3-3.py
+++ standalone_py_files/FoodViz_NN_EffNetB0_v3-3.py
@@ -1,8 +1,9 @@
 # FoodVision w/ EfficientNetB0 (internal layers frozen) - Version 3-3
 # Not an actual standalone script, meant to be run in parts in a notebook (this is for display purposes;)
 
+# -------------------------------------------------------------------- #
 # Imports
-
+# -------------------------------------------------------------------- #
 import torch
 from torchvision import models, datasets, transforms
 from torch.utils.data import DataLoader
@@ -15,8 +16,9 @@ from sklearn.metrics import classification_report, confusion_matrix, ConfusionMa
 from datetime import datetime
 from matplotlib import pyplot as plt
 
+# -------------------------------------------------------------------- #
 # Device Agnostic Code (MacOS friendly)
-
+# -------------------------------------------------------------------- #
 if torch.cuda.is_available():
     device = "cuda" # Use NVIDIA GPU (if available)
 elif torch.backends.mps.is_available():
@@ -26,12 +28,56 @@ else:
     
 print(device)
 
+# -------------------------------------------------------------------- #
 # Google Colab Mounting (if running in Google Colab)
-# from google.colab import drive
+# -------------------------------------------------------------------- #
+# # from google.colab import drive
 # drive.mount('/content/drive')
 
-
+# -------------------------------------------------------------------- #
 # Load the data from the drive (local): *(assuming these are already downloaded and unzipped)*
+# -------------------------------------------------------------------- #
 train_dir = "./data/pizza_steak_sushi/train/"
 test_dir = "./data/pizza_steak_sushi/test/"
 
+# -------------------------------------------------------------------- #
+# Load EfficientNet with B0 pretrained weights
+# -------------------------------------------------------------------- #
+weights_eff = models.EfficientNet_B0_Weights.IMAGENET1K_V1
+model_eff = models.efficientnet_b0(weights=weights_eff)
+preprocess_eff = weights_eff.transforms()
+
+
+# -------------------------------------------------------------------- #
+
+# -------------------------------------------------------------------- #
+
+
+
+# -------------------------------------------------------------------- #
+
+# -------------------------------------------------------------------- #
+
+
+
+# -------------------------------------------------------------------- #
+
+# -------------------------------------------------------------------- #
+
+
+
+# -------------------------------------------------------------------- #
+
+# -------------------------------------------------------------------- #
+
+
+
+# -------------------------------------------------------------------- #
+
+# -------------------------------------------------------------------- #
+
+
+
+# -------------------------------------------------------------------- #
+
+# -------------------------------------------------------------------- #
